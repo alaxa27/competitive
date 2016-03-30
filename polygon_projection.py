f60f1470ba12e83e483127c109d1f60f22b88130
SAKMECHE Taqiy <alaxa2@gmail.com>
2016-03-30 12:09:08 +0200

    modifié:         polygon_projection.py

--- polygon_projection.py
+++ polygon_projection.py
@@ -23,38 +23,54 @@ class PolygonProjections:
     def make_vecs(self, V):
         return [np.asarray(V)[:, 0], np.asarray(V)[:, 1]] 
 
-    def rotate(self, theta, vectors):
+    def rotate(self, theta, vectors, point):
         R_theta = np.matrix([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
-        Rc = np.dot(R_theta, self.Center)
+        Rc = np.dot(R_theta, point)
         V = []
         for v in vectors:
-            vprim = np.dot(R_theta, v) - Rc + self.Center 
+            vprim = np.dot(R_theta, v) - Rc #+ point#+ self.Center 
             vprim = np.squeeze(np.asarray(vprim))
             V.append(vprim)
         return V
 
 P = PolygonProjections()
 
-[x, y] = P.make_vecs(P.rotate(np.pi/2, P.Vectors))
-[a, b] = P.make_vecs(P.rotate(np.pi/4, P.Vectors))
+"""
+print P.Vectors[2]
 
-
-for i in range(1, 20):
-    A = P.rotate(i*np.pi/20, P.Vectors)
+D = []
+F = []
+for i in range(1, 40):
+    A = P.rotate(i*np.pi/20, P.Vectors, P.Vectors[1])
     [a, b] = P.make_vecs(A)
-    print a 
-    print b
-    #print np.sum(a)
-    #print np.sum(b)
-    #print A
-    print [i[0]+i[1] for i in A]
-    #print np.sum(np.abs(a))
-    #print np.sum(np.abs(b))
+    D.append(np.sum(a))
+    F.append(np.sum(b))
     plt.plot(a, b, 'ro')
     plt.draw()
-    plt.axis([-0.5, 1.5, -0.5, 1.5])
+    plt.axis([-3, 3, -3, 3])
 plt.plot(P.X, P.Y, 'bo')
 plt.draw()
+d = max(D)
+f = max(F)
+max = max(d, f)
+min = min(e, r)
 
 plt.show()
+"""
+MAX = []
+MIN = []
+for j in P.Vectors:
+    for i in range(0, 20):
+        A = P.rotate(i*np.pi/10, P.Vectors, j)
+        [a, b] = P.make_vecs(A)
+        MAX.append([max(np.sum(a), np.sum(b)), j, i])
+        MIN.append([min(np.sum(a), np.sum(b)), j, i])
+
+    for i in range(0, 2*n):
+        A = P.rotate(i*
+    if m > M:
+        M = m
+    else:
+        look_for(i-1, 10*n) 
+print str(max(MIN)) + " " + str(max(MAX)) 
